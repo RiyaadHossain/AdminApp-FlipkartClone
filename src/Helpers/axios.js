46925b65041746c287a973f7d7989a090eb8d885
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-08-21 15:39:02 +0600

    added interceptors in axios

--- src/Helpers/axios.js
+++ src/Helpers/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios"
+import store from "../Store/index"
 import { apiConfiq } from "../apiConfiq"
+import { authContants } from "../Actions/constants"
 
 const token = window.localStorage.getItem("token")
 
@@ -8,6 +10,23 @@ const axiosAPI = axios.create({
     headers: {
         "Authorization": token ? `Bearer ${token}` : ""
     }
+})
+
+axiosAPI.interceptors.request.use(req => {
+    const { auth } = store.getState()
+    if (auth.token) return req.headers.Authorization = `Bearer ${auth.token}`
+    return req
+})
+
+axiosAPI.interceptors.response.use(res => {
+    return res
+}, err => {
+    const { status } = err.response
+    if (status === 500 || status === 400) {
+        localStorage.clear()
+        store.dispatch({ type: authContants.LOGOUT_SUCCESS })
+    }
+    return Promise.reject(err)
 })
 
 export default axiosAPI
